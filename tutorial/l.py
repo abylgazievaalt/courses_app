3f149186f62b6e56244d41ac0face47051fb7741
Altynai <abylgazievaalt@gmail.com>
2019-07-19 16:28:00 +0600

    modified procfile

--- tutorial/l.py
+++
@@ -1,9 +0,0 @@
-from snippets.models import *
-from snippets.serializers import *
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-
-course = Course.objects.create(name='Python for everyone', description='University of Michigan')
-Branch.objects.create(course=course, latitude='12.34', longtitude='34.56', address='Aaly Tokombaev, 45')
-serializer = CourseSerializer(instance=course)
-serializer.data
